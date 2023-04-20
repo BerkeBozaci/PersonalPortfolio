b1c1716df491f272c669817e7b12747d4498e57d
Berke Bozacı <bbozaci20@ku.edu.tr>
2023-04-20 06:16:28 +0300

    Experience css editted, educations created

---
+++ personal-website/src/components/Education/Education.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Educations.css";
+
+type Props = {
+  title: string;
+  fromDate: string;
+  duration: string;
+  location: string;
+  description: string;
+};
+
+function Education({
+  title,
+  fromDate,
+  duration,
+  location,
+  description,
+}: Props) {
+  return;
+}
+
+export default Education;
---
+++ personal-website/src/components/Education/Educations.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Educations() {
+  const educations = [
+    {
+      title: "Bachelor of Engineering",
+      fromDate: "2022",
+      duration: "Present",
+      location: "Koç University",
+      description: "Electrical & Electronical Engineering",
+    },
+    {
+      title: "Bachelor of Engineering",
+      fromDate: "2020",
+      duration: "Present",
+      location: "Koç University",
+      description: "Computer Engineering",
+    },
+    {
+      title: "High School Degree",
+      fromDate: "2016",
+      duration: "2020",
+      location: "Edirne Bahçeşehir High School",
+      description: "",
+    },
+  ];
+}
+
+export default Educations;
